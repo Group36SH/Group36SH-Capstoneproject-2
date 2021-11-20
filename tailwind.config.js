a67a1ccfb537873c4d3a15e4dd883d7b8c97fdab
Ask4mike <mikaelakpang@gmail.com>
2021-11-20 03:02:12 +0100

    Added all Tailwindcss files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: "#F2E22A",
+        light_purple: "#D54094",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
