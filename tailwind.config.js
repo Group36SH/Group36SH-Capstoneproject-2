fa509d0c734c3b32ce65ec01555e13ec6b17a14a
Ask4mike <mikaelakpang@gmail.com>
2021-11-23 14:31:44 +0100

    added the images files
    modified html css and tailwindcss config.files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gray: "#F2E22A",
-        light_purple: "#D54094",
+        // primary colors
+        Soft_red: "#fe7867",
+        bright_yellow: "	#fad400",
+        bright_blue: "#6ec9fe",
+        // graphic design text
+        Dark_desaturated_cyan: "#25564b",
+        // photography text
+        Dark_blue: "#19536b",
+        // footer
+        Dark_moderate_cyan: "#458c7e",
+
+        // Neutral
+        Very_dark_desaturated_blue: "#23303e",
+        Very_dark_grayish_blue: "#5a636c",
+        Dark_grayish_blue: "#818498",
+        Grayish_blue: "#a7abae",
+        White: "#ffffff",
       },
     },
   },
