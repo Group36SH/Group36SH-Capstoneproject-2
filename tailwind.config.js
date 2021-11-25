c6d10b1e4ea62383c3c7efdb06bf82216567a7ff
Ask4mike <mikaelakpang@gmail.com>
2021-11-25 06:15:18 +0100

    modified all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,29 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        'barlow': ['Barlow', 'sans-serif'],
+        'fraunces': [ 'Fraunces', 'serif' ],
+        'poppins': [ 'Poppins', 'sans-serif' ],
+      },
+      backgroundImage: {
+        'hero-section': "url('./images/desktop/image-header.jpg')",
+        'transform': "url('./images/desktop/image-transform.jpg')",
+        'stand-out': "url('./images/desktop/image-stand-out.jpg')",
+        'graphic-design': "url('./images/desktop/image-graphic-design.jpg')",
+        'photography': "url('./images/desktop/image-photography.jpg')",
+        'milkbottles': "url('./images/desktop/image-gallery-milkbottles.jpg')",
+        'orange': "url('./images/desktop/image-gallery-orange.jpg')",
+        'cone': "url('./images/desktop/image-gallery-cone.jpg')",
+        'sugarcubes': "url('./images/desktop/image-gallery-sugarcubes.jpg')",
+
+      },
       colors: {
         // primary colors
         Soft_red: "#fe7867",
         bright_yellow: "	#fad400",
         bright_blue: "#6ec9fe",
+        bright_bg: "#3ebfff",
         // graphic design text
         Dark_desaturated_cyan: "#25564b",
         // photography text
@@ -27,7 +45,11 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      variants: {
+        backgroundColor: ['responsive', 'hover', 'focus', 'active', 'before'],
+      },
+    },
   },
   plugins: [],
 };
